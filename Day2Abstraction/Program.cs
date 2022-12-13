cd7f1660e7363550fa78fc6c3ad6543a1876119a
imjays <jpkumar541@gmail.com>
2022-12-13 12:09:30 +0530

    Add project files.

---
+++ Day2Abstraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+    class Abc
+    {
+        public void Fun1()
+        {
+            Console.WriteLine("Fun1 Invoked");
+        }
+        protected void Fun2()
+        {
+            Console.WriteLine("Fun2 Invoked");
+        }
+        private void Fun3() {
+            Console.WriteLine("Fun3 Invoked");
+        }
+    }
+}
